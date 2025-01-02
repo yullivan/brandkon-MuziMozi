3d2a29a1c8c86c0bccfa3034308be7b055811255
Mozi <seung5727@naver.com>
2025-01-02 15:31:03 +0900

    feat: 카테고리 내에서 판매량을 기준으로 상위 5개의 상품만 조회하는 쿼리 메서드 추가

--- src/main/java/brandkon/product/ProductRepository.java
+++ src/main/java/brandkon/product/ProductRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByBrandId(Long brandId);
+    List<Product> findByBrand_Category_Id(Long categoryId);
+    List<Product> findTop5ByBrand_Category_IdOrderBySalesDesc(Long categoryId);
 }
