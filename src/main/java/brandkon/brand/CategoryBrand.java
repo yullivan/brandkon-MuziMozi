f46af00d218661f812036fb2e4de6d3231f1d9f8
Mozi <seung5727@naver.com>
2025-01-02 17:15:31 +0900

    feat: Brand와 Category 사이의 중간 Entity 추가

---
+++ src/main/java/brandkon/brand/CategoryBrand.java
@@ -0,0 +1,52 @@
+package brandkon.brand;
+
+import brandkon.category.Category;
+import jakarta.persistence.*;
+
+@Entity
+public class CategoryBrand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    public CategoryBrand() {
+    }
+
+    public CategoryBrand(Category category, Brand brand) {
+        this.category = category;
+        this.brand = brand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+}
