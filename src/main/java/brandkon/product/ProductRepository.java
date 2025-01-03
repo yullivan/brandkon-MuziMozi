92a0841b64056e2665fc13af284a72f41e5c24aa
Mozi <seung5727@naver.com>
2025-01-03 17:05:31 +0900

    refactor: 브랜드와 카테고리 사이의 중간 Entity 이름을 BrandCategory로 변경
    중간 Entity는 Brand와 더 밀접한 관계를 갖기 때문에

--- src/main/java/brandkon/product/ProductRepository.java
+++ src/main/java/brandkon/product/ProductRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByBrandId(Long brandId);
     @Query("SELECT p FROM Product p WHERE p.brand.id IN " +
-            "(SELECT cb.brand.id FROM CategoryBrand cb WHERE cb.category.id = :categoryId)")
+            "(SELECT bc.brand.id FROM BrandCategory bc WHERE bc.category.id = :categoryId)")
     List<Product> findProductsByCategoryId(@Param("categoryId") Long categoryId);
     @Query("SELECT p FROM Product p WHERE p.brand.id IN " +
-            "(SELECT cb.brand.id FROM CategoryBrand cb WHERE cb.category.id = :categoryId) " +
+            "(SELECT bc.brand.id FROM BrandCategory bc WHERE bc.category.id = :categoryId) " +
             "ORDER BY p.sales DESC")
     List<Product> findTop5ByCategoryIdOrderBySalesDesc(@Param("categoryId") Long categoryId, Pageable pageable);
     List<Product> findTop5ByBrand_IdOrderBySalesDesc(Long brandId);
